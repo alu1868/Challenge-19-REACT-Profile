a48e9bde5f0cf408119e2b8bc0b9399608dab3c9
alu1868 <alu1868@gmail.com>
2022-07-24 15:30:28 -0500

    work portfolio outline

---
+++ src/components/Work/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Project from "../Project"
+
+function Work() {
+    const projectArray = [
+        {
+            // image:"",
+            name: "Covid Risk Tracking Site",
+            description: "A covid tracking site, that details covid statistics in a given country",
+            url: "https://github.com/alu1868/covid-risk-tracking-site"
+        },
+        {
+            // image:"",
+            name: "README Generator",
+            description: "Command line application to generate a professional outline of a typical README document for projects",
+            url: "https://github.com/alu1868/README-GENERATOR"
+        },
+        {
+            // image:"",
+            name: "Budget Tracker",
+            description: "Progressive Web Application that tracks expenses, built in chart to better visualize spending habits",
+            url: "https://github.com/alu1868/Budget-Tracker"
+        },
+        {
+            // image:"",
+            name: "Tavern Masters",
+            description: "A full MERN Stack Project, helps first time table top RPG players construct one shot campaigns from a deck of random events",
+            url: "https://github.com/alu1868/tavern-masters"
+        },
+        {
+            // image:"",
+            name: "Note-Taker",
+            description: "A simple note taking application, create, read, update, and delete notes to better oragnize your life",
+            url: "https://github.com/alu1868/Note-Taker"
+        },
+        {
+            // image:"",
+            name: "Ecommerce-Backend",
+            description: "A backend to a typical ecommerce site using MySQL2",
+            url: "https://github.com/alu1868/Ecommerce-Backend"
+        },
+        {
+            // image:"",
+            name: "Day Planner",
+            description: "An Application designed to help organize your thoughts and notes in a given work day",
+            url: "https://github.com/alu1868/Day-Planner"
+        },
+        {
+            // image:"",
+            name: "Employee Tracker",
+            description: "A command line application meant to generate a database to organize employees using MySQL2",
+            url: "https://github.com/alu1868/Employee-Tracker"
+        }
+    ]
+
+    return (
+        <section>
+            <div>
+                <div>
+                {projectArray.map((project) => 
+                    <Project image={project.image} name={project.name} description={project.description} url={project.url} />
+                  )}
+                </div>
+            </div>
+        </section>
+    )
+}
